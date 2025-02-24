77eab9086be1dd40516c71b53930f3fdb6e2ffb2
james green <jamesgreen.311@gmail.com>
2025-02-24 09:36:10 -0500

    Add new HTML structure and CSS styles for membership application; remove outdated UI files

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -1,4 +1,5 @@
-const EP_APPLICATION = "application-url-goes-here"
+const EP_APPLICATION =
+   "https://script.google.com/macros/s/AKfycbw1rT82b4rwifGSWEIuLDtXly6IAoiWASnTaLkPqHkodup0YOKh57fUnpxEWghH3aIX/exec"
 const EP_JURY = "jury-url-goes-here"
 const TEST_EP_APPLICATION =
    "https://script.google.com/macros/s/AKfycbxnpWX3jXnfHKp079sCKFkow48s-isVIU4V6k_hsbg/dev"
@@ -11,7 +12,7 @@ function showYear() {
 }
 
 function fetchSettings(query) {
-   const url = `${TEST_EP_APPLICATION}?q=${query}`
+   const url = `${EP_APPLICATION}?q=${query}`
    fetch(url)
       .then((res) => res.json())
       .then((data) => {
